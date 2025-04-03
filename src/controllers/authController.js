25533215e176dcd1c921d50d163c00541bd75139
NatiF <65537815+OptimaLPro@users.noreply.github.com>
2025-04-03 20:29:58 +0300

    Update JWT token expiration from 1 hour to 7 days

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const createToken = (user) => {
   return jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
+    expiresIn: "7d",
   });
 };
 
